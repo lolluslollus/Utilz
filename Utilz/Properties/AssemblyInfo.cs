c1e84d6cf9f1f7e9fc2f8299ad1a0afcd0be1b25
lolluslollus <lolluslollus@hotmail.com>
2017-05-14 22:00:46 +0200

    csproj updated

--- Utilz/Properties/AssemblyInfo.cs
+++ Utilz/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Utilz")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Lollus 2015, 2016, 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
