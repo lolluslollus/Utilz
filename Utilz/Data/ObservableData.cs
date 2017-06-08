c169a403b034e720878685a0f5a4efaa58e1c110
lolluslollus <lolluslollus@hotmail.com>
2017-06-08 23:03:34 +0200

    smarter logging

--- Utilz/Data/ObservableData.cs
+++ Utilz/Data/ObservableData.cs
@@ -9,139 +9,142 @@ using Windows.UI.Core;
 
 namespace Utilz.Data
 {
-	[DataContract]
-	public abstract class ObservableData : INotifyPropertyChanged
-	{
-		#region INotifyPropertyChanged
-		public event PropertyChangedEventHandler PropertyChanged;
+    [DataContract]
+    public abstract class ObservableData : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
         protected bool IsAnyoneListening()
         {
-            return PropertyChanged != null;
+            var pc = PropertyChanged;
+            if (pc == null) return false;
+            Logger.Add_TPL($"ObservableData.IsAnyoneListening found listeners: {pc?.GetInvocationList()?.ToString() ?? "NONE"}", Logger.AppEventsLogFilename);
+            return pc != null;
         }
         protected void ClearListeners()
-		{
-			PropertyChanged = null;
-		}
+        {
+            PropertyChanged = null;
+        }
 
-		protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-		}
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
-		/// <summary>
-		/// Runs in the UI thread if available, otherwise queues the operation in it.
-		/// </summary>
-		/// <param name="propertyName"></param>
-		protected void RaisePropertyChanged_UI([CallerMemberName] string propertyName = "")
-		{
-			if (PropertyChanged == null) return;
-			Task raise = RunInUiThreadAsync(delegate
-			{
-				RaisePropertyChanged(propertyName);
-			});
-		}
-		protected void RaisePropertyChangedUrgent_UI([CallerMemberName] string propertyName = "")
-		{
-			if (PropertyChanged == null) return;
-			try
-			{
-				Task raise = CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, delegate
-				{
-					RaisePropertyChanged(propertyName);
-				}).AsTask();
-			}
-			catch (InvalidOperationException) // called from a background task: ignore
-			{ }
-			catch (Exception ex)
-			{
-				Logger.Add_TPL(ex.ToString(), Logger.PersistentDataLogFilename);
-			}
-		}
-		#endregion INotifyPropertyChanged
-
-
-		#region UIThread
-		protected async Task RunInUiThreadIdleAsync(DispatchedHandler action)
-		{
-			try
-			{
-				if (CoreApplication.MainView.CoreWindow.Dispatcher.HasThreadAccess)
-				{
-					action();
-				}
-				else
-				{
-					await CoreApplication.MainView.CoreWindow.Dispatcher.RunIdleAsync(a => action()).AsTask().ConfigureAwait(false);
-				}
-			}
-			catch (InvalidOperationException) // called from a background task: ignore
-			{ }
-			catch (Exception ex)
-			{
-				Logger.Add_TPL(ex.ToString(), Logger.PersistentDataLogFilename);
-			}
-		}
-		protected async Task RunInUiThreadAsync(DispatchedHandler action)
-		{
-			try
-			{
-				if (CoreApplication.MainView.CoreWindow.Dispatcher.HasThreadAccess)
-				{
-					action();
-				}
-				else
-				{
-					await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Low, action).AsTask().ConfigureAwait(false);
-				}
-			}
-			catch (InvalidOperationException) // called from a background task: ignore
-			{ }
-			catch (Exception ex)
-			{
-				Logger.Add_TPL(ex.ToString(), Logger.PersistentDataLogFilename);
-			}
-		}
-		#endregion UIThread
+        /// <summary>
+        /// Runs in the UI thread if available, otherwise queues the operation in it.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void RaisePropertyChanged_UI([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged == null) return;
+            Task raise = RunInUiThreadAsync(delegate
+            {
+                RaisePropertyChanged(propertyName);
+            });
+        }
+        protected void RaisePropertyChangedUrgent_UI([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged == null) return;
+            try
+            {
+                Task raise = CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, delegate
+                {
+                    RaisePropertyChanged(propertyName);
+                }).AsTask();
+            }
+            catch (InvalidOperationException) // called from a background task: ignore
+            { }
+            catch (Exception ex)
+            {
+                Logger.Add_TPL(ex.ToString(), Logger.PersistentDataLogFilename);
+            }
+        }
+        #endregion INotifyPropertyChanged
 
 
-		#region get and set
-		protected T GetPropertyLocking<T>(ref T fldValue, object locker)
-		{
-			lock (locker)
-			{
-				return fldValue;
-			}
-		}
-		protected T GetProperty<T>(ref T fldValue)
-		{
-			return fldValue;
-		}
-		protected void SetPropertyLocking<T>(ref T fldValue, T newValue, object locker, bool raise = true, bool onlyIfDifferent = true, [CallerMemberName] string propertyName = "")
-		{
-			bool isValueChanged = false;
-			lock (locker)
-			{
-				T oldValue = fldValue;
-				if (!newValue.Equals(oldValue) || !onlyIfDifferent)
-				{
-					fldValue = newValue;
-					isValueChanged = true;
-				}
-			}
-			// separate to avoid deadlocks
-			if (isValueChanged && raise)
-			{
-				RaisePropertyChanged_UI(propertyName);
-			}
-		}
-		protected void SetProperty<T>(ref T fldValue, T newValue, bool raise = true, bool onlyIfDifferent = true, [CallerMemberName] string propertyName = "")
-		{
-			T oldValue = fldValue;
-			if (newValue.Equals(oldValue) && onlyIfDifferent) return;
+        #region UIThread
+        protected async Task RunInUiThreadIdleAsync(DispatchedHandler action)
+        {
+            try
+            {
+                if (CoreApplication.MainView.CoreWindow.Dispatcher.HasThreadAccess)
+                {
+                    action();
+                }
+                else
+                {
+                    await CoreApplication.MainView.CoreWindow.Dispatcher.RunIdleAsync(a => action()).AsTask().ConfigureAwait(false);
+                }
+            }
+            catch (InvalidOperationException) // called from a background task: ignore
+            { }
+            catch (Exception ex)
+            {
+                Logger.Add_TPL(ex.ToString(), Logger.PersistentDataLogFilename);
+            }
+        }
+        protected async Task RunInUiThreadAsync(DispatchedHandler action)
+        {
+            try
+            {
+                if (CoreApplication.MainView.CoreWindow.Dispatcher.HasThreadAccess)
+                {
+                    action();
+                }
+                else
+                {
+                    await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Low, action).AsTask().ConfigureAwait(false);
+                }
+            }
+            catch (InvalidOperationException) // called from a background task: ignore
+            { }
+            catch (Exception ex)
+            {
+                Logger.Add_TPL(ex.ToString(), Logger.PersistentDataLogFilename);
+            }
+        }
+        #endregion UIThread
 
-			fldValue = newValue;
-			if (raise) RaisePropertyChanged_UI(propertyName);
-		}
-		#endregion get and set
-	}
+
+        #region get and set
+        protected T GetPropertyLocking<T>(ref T fldValue, object locker)
+        {
+            lock (locker)
+            {
+                return fldValue;
+            }
+        }
+        protected T GetProperty<T>(ref T fldValue)
+        {
+            return fldValue;
+        }
+        protected void SetPropertyLocking<T>(ref T fldValue, T newValue, object locker, bool raise = true, bool onlyIfDifferent = true, [CallerMemberName] string propertyName = "")
+        {
+            bool isValueChanged = false;
+            lock (locker)
+            {
+                T oldValue = fldValue;
+                if (!newValue.Equals(oldValue) || !onlyIfDifferent)
+                {
+                    fldValue = newValue;
+                    isValueChanged = true;
+                }
+            }
+            // separate to avoid deadlocks
+            if (isValueChanged && raise)
+            {
+                RaisePropertyChanged_UI(propertyName);
+            }
+        }
+        protected void SetProperty<T>(ref T fldValue, T newValue, bool raise = true, bool onlyIfDifferent = true, [CallerMemberName] string propertyName = "")
+        {
+            T oldValue = fldValue;
+            if (newValue.Equals(oldValue) && onlyIfDifferent) return;
+
+            fldValue = newValue;
+            if (raise) RaisePropertyChanged_UI(propertyName);
+        }
+        #endregion get and set
+    }
 }
